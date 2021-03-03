217c21f50e39056bb6fc485bbfdc628629814a52
Neuroconductor <adig@jhu.edu>
2021-03-03 11:27:25 -0500

    Version id 1 stable

---
+++ tic.R
@@ -0,0 +1,36 @@
+# installs dependencies, runs R CMD check, runs covr::codecov()
+do_package_checks(args=c("--as-cran","--install-args=--build"))
+
+InstallRemoteBinaries <- R6Class(
+  "InstallRemoteBinaries",
+  inherit = TicStep,
+
+  public = list(
+    run = function() {
+      # get remotes and try to install binaries
+      remotes = ghtravis::get_remotes()
+
+      if(!is.null(remotes)) {
+        for(remote in remotes) {
+          remote_info <- ghtravis::parse_remotes(remote)
+          github_user <- sapply(remote_info, `[[`, "username")
+          repo <- sapply(remote_info, `[[`, "repo")
+          slug <- paste0(paste0(github_user,"/",repo))
+
+          if(!is.null(github_user) && !is.null(repo)) {
+            message(paste0("Installing ",repo," binaries"))
+            ghtravis::install_remote_binaries(check_r_version = TRUE, force_sha = FALSE, remotes = c(slug))
+          }
+        }
+      }
+    }
+  )
+)
+
+step_install_remote_binaries <- function() {
+  InstallRemoteBinaries$new()
+}
+
+get_stage("before_install") %>%
+add_step(step_install_remote_binaries())
+
